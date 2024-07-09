b947c7969a105bb01b1d46643a34c7eb21a6c088
Miguel Coelho <miguel.pinheiro.coelho@tecnico.ulisboa.pt>
2024-07-09 20:42:15 +0100

    redid continent script god it was so shit

--- continent_helper.py
+++ continent_helper.py
@@ -1,71 +1,79 @@
 import string
 
-def get_provinces_from_area(area: str):
-    with open('map/area.txt', 'r') as areas:
-        lines = areas.readlines()
-        for i in range(0, len(lines)):
-            if lines[i].startswith(area):
-                return lines[i+1].split()
+import pandas as pd
+import csv
+import numpy as np
 
-def get_areas_from_region(region: str):
-    areas = []
-    in_place = False
-    with open('map/region.txt', 'r') as regions:
-        lines = regions.readlines()
-        for i in range(0, len(lines)):
-            if in_place and lines[i].startswith("areas"):
+# [Options] may be useful for debugging
+pd.options.display.max_rows = 9999
+pd.set_option('display.max_columns', None)
+pd.set_option('display.expand_frame_repr', False)
+pd.set_option('max_colwidth', 800)
+
+# [Setup] leave as is, please
+SHEET_ID = "1Hj_YOoVy9d6G0wkMezla6iDy7kvkiAXWyKNEhiECAWE"
+SHEET_NAME = "Provinces"
+url = f'https://docs.google.com/spreadsheets/d/{SHEET_ID}/gviz/tq?tqx=out:csv&sheet={SHEET_NAME}'
+df = pd.read_csv(url)
+df = df.drop(df.columns[30:41], axis = 1)
+df.columns = ["name", "id", "type", "rgb", "area", "region", "superregion", "continent", "winters", "monsoons", "terrain", "climate", "is_colonized", "is_owned_by", "is_core_of", "is_city", "religion", "culture", "tradenode", "tradegood", "latentgood", "cot_rank", "base_tax", "base_production", "base_manpower", "total_development", "has_lv2_fort", "discovered_by", "prov_modifiers", "notes"]
+df = df.dropna(axis=0, subset=['type'])
+
+# [Config] please add all recognizable continents to this!!
+continent_map = {
+    "Alteniquia": "alteniquia",
+    "Nieden": "nieden",
+    "Syltor": "syltor",
+    "Sparwood": "sparwood",
+    "Haven": "haven",
+    "The North": "the_north",
+    "Valdreach": "valdreach",
+    "Plagos": "plagos",
+    "Akan'nash": "akannash"
+}
+
+# [Logic] where the magic happens
+provlist_mapping = {}
+for continent in continent_map.values():
+    provlist_mapping.update({continent: []})
+
+i = -1
+last_province = 0
+with open('map/definition.csv', 'r', encoding='UTF-8') as definition:
+    for line in definition.readlines():
+        i = i + 1
+        line_arr = line.split(";")
+        if not line_arr[0].isnumeric():
+            print(line)
+            continue
+        else:
+            provID = int(line_arr[0])
+            if last_province == 0 and i != provID:
+                last_province = i-1
+            if not df.at[provID-1, 'type'] == "Land":
                 continue
-            if in_place:
-                j = 0
-                while True:
-                    ix = i + j
-                    if lines[ix].startswith("}"):
-                        return areas[1:-1]
-                    elif not lines[ix].isspace() and not lines[ix].startswith("areas"):
-                        areas.append(lines[ix][:-6].strip())
-                    j = j + 1
-            if lines[i].startswith(region):
-                in_place = True
-                continue
-            
-def get_regions_from_superregion(superregion: str):
-    in_place = False
-    regions = []
-    with open('map/superregion.txt', 'r') as superregions:
-        lines = superregions.readlines()
-        for i in range(0, len(lines)):
-            if in_place:
-                j = 0
-                while True:
-                    ix = i + j
-                    if lines[ix].startswith("}"):
-                        return regions[1:]
-                    elif not lines[ix].startswith("\trestrict_charter"):
-                        regions.append(lines[ix][1:-8])
-                    j = j+1
-            if lines[i].startswith(superregion):
-                in_place = True
-                continue
+            print("debug: we are in " + line_arr[4])
+            continent_str = str(df.at[provID-1, 'continent'])
+            continent = continent_map[continent_str]
+            prov_list = provlist_mapping[continent]
+            prov_list.append(provID)
 
-def prepare_statement_for_continent(continent_name: str, continent: list):
-    statement = continent_name + " = {\n"
-    regions = []
-    for superregion in continent:
-        regions = get_regions_from_superregion(superregion)
-        for region in regions:
-            print(region)
-            statement = statement + "\t# " + string.capwords(region.replace("_", " ")) + "\n"
-            provs_total = []
-            areas = get_areas_from_region(region)
-            for area in areas:
-                provs_total = provs_total + get_provinces_from_area(area)
-            provs_string = ' '.join([str(prov) for prov in provs_total])
-            statement = statement + "\t" + provs_string + "\n"
-    statement = statement + "}" + "\n"
-    return statement
+with open('map/continent.txt', 'w', encoding='ISO-8859-1') as result:
+    for continent in provlist_mapping:
+        statement = continent + " = {\n"
+        prov_list = provlist_mapping[continent]
+        provs_string = ' '.join([str(prov) for prov in prov_list])
+        statement = statement + "\t" + provs_string + "\n"
+        statement = statement + "}" + "\n\n"
 
+    base_game_section = "### Base Game\neurope = {\n\n}\n\nasia = {\n\n}\n\nafrica = {\n\n}\n\nnorth_america = {\n\n}\n\nsouth_america = {\n\n}\n\noceania = {\n\n}\n\n"
+    
+    debug_continent_section = "debug_continent = { #unused province ids go here to prevent errors. if an id is present in 2 continents, it will create an error, so they're easy to find\n"
+    debug_string = ' '.join([str(id) for id in range(last_province+1, 5000)])
+    debug_continent_section = debug_continent_section + '\t' + debug_string + '\n'
+    debug_continent_section = debug_continent_section + "}\n\n"
 
-alteniquia = ["essukdal", "aldovir", "vastallos"]
-nieden = ["varmerien", "asterien"]
-sparwood = ["western_sparwood"]
-print(prepare_statement_for_continent("nieden", nieden))
+    last_section = "island_check_provinces = {\n\n}\n\n# Used for RNW\nnew_world = {\n\n}\n"
+
+    statement = statement + base_game_section + debug_continent_section + last_section
+    result.write(statement)
